897d7a05073aa7deb5eccd1299834843a2572094
Romashka-Al <alinashkunova@yandex.ru>
2024-08-08 13:24:09 +0400

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as fake_div
+from true_math import divide as true_div
+
+
+print(fake_div(228, 1))
+print(fake_div(17, 0))
+print(true_div(55, 5))
+print(true_div(73, 0))
